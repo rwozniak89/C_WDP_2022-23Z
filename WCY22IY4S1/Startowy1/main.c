7bdb0efa91ff617e09379ddbf9512bdfdd3b9cee
RadekWoz <rwozniak89@gmail.com>
2022-11-07 18:28:38 +0100

    task2

--- WCY22IY4S1/Startowy1/main.c
+++ WCY22IY4S1/Startowy1/main.c
@@ -3,7 +3,17 @@
 
 int main()
 {
+
+
     printf("Hello world Startowy1!\n");
+
+//
+//1. kalkulator podstawowych dzia³añ. Zmienne zdefiniowane i zadeklarowen w kodzie.
+//2 zmienne calkowie
+//2 zmienne zmiennorzecikowe
+//Dzialania dod, ode, dziel, mnoz, modulo, z wyswietleniem na konsoli
+//
+//Wykonanie 5 podstawowych dzia³an  (1pkt)
 
     int a=5, b=3;
     int wynik_i = 0;
@@ -11,13 +21,17 @@ int main()
 
 
     wynik_i = a + b;
-    printf("Wynik %i\n", wynik_i);
+    printf("Wynik %i\n", wynik_i, wynik_f );
 
     if(b != 0){
         wynik_f = ((a)/ ((float) b));
     }
 
-     printf("Wynik %i , wynik kolejny %f \n", wynik_i, wynik_f );
+     printf("Wynik %i , wynik kolejny %f \n", wynik_i, wynik_f );  //%.2f
+
+
+
+
 
     return 0;
 }
--- WCY22IY4S1/Startowy2/main.c
+++ WCY22IY4S1/Startowy2/main.c
@@ -4,5 +4,30 @@
 int main()
 {
     printf("Hello world Start2 !!!!!!!###########\n");
+
+  //  2. wyœwietlanie 10 kolejnych liczb naturalnych, i sprawdzenie ka¿dej czy jest parzysta/nieparzysta (1pkt)
+
+
+
+    for(int i =0 ; i<10 ; i++ )
+    {
+        printf("%d \n", i);
+    }
+
+    int i=0;
+    for( ; ; )
+    {
+
+        printf("%d \n", i);
+
+        i++;
+
+        if(i>=10)
+        {
+            break;
+        }
+
+    }
+
     return 0;
 }
