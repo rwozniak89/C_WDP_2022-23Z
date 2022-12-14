3f8be188fa9f610a14c4bfd5ee656c950684107a
RadekWoz <rwozniak89@gmail.com>
2022-12-14 18:27:47 +0100

    update

--- WCY22IY4S1/Lab4/Zad3/main.c
+++ WCY22IY4S1/Lab4/Zad3/main.c
@@ -4,14 +4,12 @@
 
 
 int zwrocParzystosc(int liczba){
-
     if(liczba%2==0) {
         return 0;
     }
     //else{
         return 1;
     //}
-
     //return i%2==0 ? 0 : 1;
     //return i%2==0
 }
@@ -23,17 +21,13 @@ void wyswietlParzystosc(int liczba){
     else{
         printf("%d - liczba nieparz\n", liczba);
     }
-
 }
 
 int wyswietlIZwrocParzystosc(int x, int  sterowanie){
-
     if(sterowanie != 0){
         wyswietlParzystosc( x);
     }
-
     int wynikOdebrany = zwrocParzystosc( x);
-
     printf("wynikOdebrany: %d\n",wynikOdebrany);
 
     return wynikOdebrany;
